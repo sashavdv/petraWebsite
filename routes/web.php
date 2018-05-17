8f85805423794c3a3d758e3407978728d43fe213
sashavdv <sashavandevoorde@gmail.com>
2018-05-17 14:05:15 +0200

    added login

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/contact', function () {
 Route::post('/contact', function () {
     return view('pages.contact');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
