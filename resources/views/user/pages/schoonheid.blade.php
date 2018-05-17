2d663b0717d93a06a853430f5e27e19c10ff660a
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-05-17 14:31:46 +0200

    Revert "h"
    This reverts commit ac6368f3d32060e3748e9200010983b9d36c540a, reversing
    changes made to 5fb9de26683c1bc47752f0e4350886095ef53e76.

--- resources/views/user/pages/schoonheid.blade.php
+++ resources/views/user/pages/schoonheid.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('template.layout')
 @section('content')
     <h1>Gelaatsverzorging</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vulputate arcu nisl, sed euismod elit cursus eget. Nunc fringilla dui aliquam, tempor nisi vel, dictum lectus. Maecenas in ligula at lacus interdum aliquet. Sed mollis consequat arcu, quis luctus orci rutrum in. Nulla pulvinar, tortor id suscipit sagittis, magna velit rutrum mi, id vehicula tortor neque sit amet nunc. Ut auctor, nisl eu auctor dapibus, elit lorem pellentesque nunc, sit amet egestas ante urna eget lacus. Duis diam nisi, vestibulum vitae elit imperdiet, interdum aliquam justo. Vestibulum posuere nulla quis sollicitudin luctus. Pellentesque molestie, leo sit amet auctor porta, sapien sapien varius eros, at ornare lectus elit at justo.</p>
