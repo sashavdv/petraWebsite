84782140f7a5c2f5bf7017d460a296d12ad3e711
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-05-17 17:03:16 +0200

    Wireframe

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function sendMail(){
-        return view('pages.contact');
+    public function sendMail(Request $request){
+        
     }
 }
