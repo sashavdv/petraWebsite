4c0ce8fa6bf11dfd277230095309b91e4463fe0e
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-05-30 13:23:21 +0200

    translations

--- resources/views/user/pages/contact.blade.php
+++ resources/views/user/pages/contact.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
     <h1>@lang('titles.contact')</h1>
-    <h1>Parking</h1>
-    <h1>Maps</h1>
+    <h1>@lang('titles.parking')</h1>
+    <h1>@lang('titles.location')</h1>
     <div class="container">
         <div id="map"></div>
         <script>
@@ -22,14 +22,14 @@
     </div>
     <h1>@lang('titles.subscribe')</h1>
     {{ Form::open(['action' => 'ContactController@sendMail', 'method' => 'post']) }}
-    {{ Form::label('first-name', 'Voornaam') }}
+    {{ Form::label('first-name', __('forms.first_name')) }}
     {{ Form::text('first-name', '', ['class' => 'form-control']) }}
-    {{ Form::label('last-name', 'Achternaam') }}
+    {{ Form::label('last-name', __('forms.last_name')) }}
     {{ Form::text('last-name', '', ['class' => 'form-control']) }}
-    {{ Form::label('email', 'Email') }}
+    {{ Form::label('email', __('forms.email')) }}
     {{ Form::email('email', '', ['class' => 'form-control']) }}
-    {{ Form::checkBox('Verzenden', true, true) }}
-    {{ Form::label('subscribe', 'Ik wil e-mails ontvangen') }}
-    {{ Form::submit('Inschrijven', ['class' => 'form-control']) }}
+    {{ Form::checkBox('subscribe', true, true) }}
+    {{ Form::label('subscribe', __('forms.get_emails')) }}
+    {{ Form::submit(__('forms.subscribe'), ['class' => 'form-control']) }}
     {{ Form::close() }}
 @endsection
