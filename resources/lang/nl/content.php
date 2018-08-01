c62ad84d9aa35aa10d9fc1148d64d6de810a0421
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-08-01 16:37:15 +0200

    text petra

---
+++ resources/lang/nl/content.php
@@ -0,0 +1,138 @@
+<?php
+return [
+    /* Home */
+    'about' => '<P>
+                    Als dochter van ouders met een vroegere achtergrond in de clerus, kwam Petra al jong de tegenstellingen in georganiseerde religie en meer holistische denkbeelden tegen. 
+                    Haar ouders zetten zich ook als eerste(n) in een kleine dorpsgemeenschap ook in naar sociaal onrecht, haar moeder was erg creatief van natuur en haar vader had een goedlachs standvastig karakter, 
+                    waar zijn liefde voor het leven zich voornamelijk uitte in de genegenheid hij voor zijn vrouw en dochter koesterde.
+                </P>
+                <p>
+                    Dat haar ouders hun kerkelijk ambt aan de haak hingen en voor een gezin kozen, zorgde wel altijd voor wat deining in de dorpsgemeenschap en Petra kreeg met veelvuldige pesterijen rond dit stigma te maken. 
+                    Het maakte dat ze zich als tiener jarenlang ‘anders’ voelde en geen aansluiting vond. Het maakte echter ook dat haar ontwakingsproces op gang kwam doordat ze van veel zaken al jong de andere kant 
+                    van de medaille kon observeren.En het kweekte in kiem, hoe onaangenaam ook op dat eigenste moment, een soort van vertrouwen in zichzelf en haar eigen intuïtie. Het leerde haar anders te kijken.
+                </p>
+                <p>
+                    Tevens draaide ze in een vorm van jeugdige rebellie, alle wereldgodsdiensten wel eens op zijn kop en bleef toen een periode hangen aan het paganisme. 
+                    Al kwam die insteek oorspronkelijk eerder op gang door haar enorme voorliefde voor fytotherapie en het ritueel gebruik van geneeskrachtige en giftige planten.
+                </p>
+                <p>
+                    Toch ook daar, bij deze natuurgodsdienst, zette ze na 15 jaar kanttekeningen bij de heersende denkbeelden die evenzeer eerder richting macht stroomden, dan richting universele 
+                    liefde en liefde voor het individue.
+                </p>
+                <p>
+                    In de loop der jaren, verdiepte ze zich in het eenheidsgegeven van deze wereld, zonder daarom aan onze klassieke ‘realiteit’ van dualiteit voorbij te gaan.
+                    Wit & zwart, voor haar gaan ze, in deze realiteit, deze wereld samen. 
+                    Met als stipulatie weliswaar, dat onze leefwereld er een is waar kwantumfysica & kosmisch denken, gelukkig stilaan mainstream in ons gezamenlijk collectief doorbreekt.
+                </p>
+                <p>
+                    De kern hiervan: Zelfliefde & healing, het weten dat het al goed (zo) is, ongeacht de omstandigheden van onze persoonlijke levens, is wat ze wilt uitdragen.
+                    Voor deze hedendaagse maatschappij, is dat gegeven van verbinding, van zoveel mogelijk echtheid, haar gift. 
+                </p>
+
+                <p>
+                    Petra is van opleiding een gedreven schoonheidsspecialiste met een specialisatie in cosmetologie, aromatherapie, natuurlijke huidverzorging, cosmetische en ontspanningsmassage en medische pedicure.
+                    Kleur, geur en natuurlijke fleur, doen haar hart sneller slaan. 
+                </p>
+                <p>
+                    Je kan bij haar terecht voor persoonlijk advies rond je  gevoelige- of probleemhuid + littekenbehandeling na plastische chirurgie of operaties.
+                    Een gelaatsverzorging zal altijd voorafgegaan worden door een grondige huidanalyse, en de gebruikte extracten en producten worden ter plekke aan jouw huidtype aangepast.
+                    Heb je probleem voeten, of gewoon moeilijk te verzorgen voeten of teennagels, dan kan je bij haar genieten van een pedicure die verder gaat dan nagels knippen en velletjes & eelt verwijderen. 
+                </p>
+                <p>
+                    Tevens biedt Petra je veelvuldige meditatietechnieken aan, en ook op het gebied van spiritualiteit kan je bij haar terecht. Balancering door middel van chakrawerk is een specialiteit.
+                </p>
+                <p>
+                    Of voel je je aangetrokken tot de werking van de Tarot en zijn omstandige, mystieke symboliek? Heb je eerder een boon voor de vrijmoedige Lenormandkaarten?
+                    Petra geeft regelmatig info hierover vrij, en er staat jaarcursus op het programma voor  diegenen die zich hierin willen verdiepen. 
+                </p>
+                <p>
+                    De modus in spiritueel onderzoek en divinatie is altijd gericht op een groter begripsvermogen en bijgaande inzichtelijke puurheid, die reikt naar wat ons doet tikken als mens, naar dat wat 
+                    onze harten oplicht en verblijd. 
+                </p> 
+                <p>
+                    Schaduwonderzoek wordt hierbij niet vermeden daar perfectionisme naar constante vrolijkheid, een utopie is die vaak maar ontduikingsgedrag voorstelt.
+                    We zijn licht én duister in haar ondervinding, al vallen deze beiden sowieso onder de noemer LIEFDE. Instincten en miljoenen jaren celgeheugen laten zich op dit vlak wel degelijk gelden, 
+                    toch zijn wij mensen als energetische entiteit zoveel groter en meer dan we zelf vaak beseffen.
+                </p>
+                <p>
+                    Gelukkig zijn is voor Petra een balans(oefening) die je kan leren, iets waar ze zich graag mee naar het collectief toe, ten dienste stelt.
+                    Wees gerust, we zijn nooit klaar, en we never get it ‘done’. 
+                    Mens zijn betekent voor haar intrinsiek impecfectie. Datgene wat net zo mooi, ontwapenend en spannend kan zijn.
+                    Intuïtieve kracht in jezelf vinden, dicht bij jezelf blijven - maw het goddelijke aspect van jezelf eren, datgene wat zo moeilijk onder woorden te brengen is, is wat zij op een DIY 
+                    manier inbouwt in haar leven. Gelijk ook hetgene zij uitdraagt en waar ze allerlei manieren voor aanreikt als houvast.
+                </p>
+                <p>
+                    Haar laatste fascinatie geldt de verschillen in man- vrouw energie. En hoe we als maatschappij en individue kunnen terugkomen van het #METOO verhaal.
+                    Maw: Hoe geraken een man en een vrouw nader?
+                    Liefde beleven in het post feministische tijdperk - waar de eerste uitwassen van deze maatschappelijk (nodige) stroming blijken  – is niet voor watjes of bange zieltjes. Je hart staat op het 
+                    spel en we begrijpen elkaar vaak niet…
+                </p>
+                <p>
+                    Petra is nu 45, heeft een leven van onderzoek erop zitten, baarde 2 prachtige kinderen – Jasper & Jasmijn en is pas gehuwd met een man voor wie ze haar spirituele inslag en haar hang naar esthetiek 
+                    en werkelijke schoonheid, niet moet minimaliseren of langer ontkennen.
+                </p>
+                <p>
+                    Haar keuze om te kiezen voor innerlijke en uiterlijke schoonheid en esthetiek, balancering en ontspanningsvermogen in combinatie in-zichtelijkheid via divinatie, 
+                    leest na dit relaas waarschijnlijk als de logica zelve.
+                    Haar MISSION STATEMENT in life is dan ook Fun, Freedom, Helping others and Love.
+                </p>
+                <p>
+                    Enkele jaren terug besliste zij om uitsluitend nog datgene te doen met haar leven, wat onder deze noemers te catalogiseren valt.        
+                </p>',
+
+    /* Beauty */
+    'face_care' => '
+                <p>Onze huid is ons grootste orgaan. En toch vergeten we vaak, dat dit een levend organisme is.</p>
+                <p>In een wereld waar we langs alle kanten overspoeld worden door toxische stoffen, wil ik je op sleeptouw nemen langs een meer natuurlijk pad voor jouw huid.</p>
+                <p>De huid van je gelaat en je lichaam.</p>
+                <p>Waarom?</p>
+                <p><ul>
+                    <li>Omdat op een meer zorgzame en verfrissende wijze met jezelf om springen, je blij maakt en krachtig je zelfbeeld ondersteunt.</li>
+                    <li>Omdat je zo een verlaging van immuniteitsziekten kan verkrijgen en het veel minder allergische reactie opwekt.</li>
+                    <li>Omdat het genetisch gezien beter voor ons allemaal is, en we onszelf én de natuur er een ferme dienst mee doen.</li>
+                    <li>Omdat niets zo makkelijk én eenvoudig is, als met natuurlijke producten en degelijke basis ingrediënten te werken of je hiermee te laten verzorgen.</li>
+                    <li>Omdat je je verzorgingsproducten zelf zo ondersteunend en activerend voor je huid kan maken als je maar wilt.</li>
+                    <li>Omdat natuurlijke geuren iets anders doen met onze hersenen dan gefabriceerde namaak.</li>
+                    <li>Omdat het nu eenmaal anders kan, en deze wereld je een speelplein van mogelijkheden geeft, dus waarom zou je jezelf daar niet van bedienen?</li>
+                    <li>Omdat alles er al is en bestaat: degelijke verzorgingen met werkelijk natuurlijke ingrediënten!</li>
+                </ul></p>
+                <p>Elke dag worden we namelijk geconfronteerd met pollutie in de lucht rond ons, met agressieve detergenten en wasproducten, met kleurstoffen en bewaarmiddelen in onze voeding en verzorgingsproducten. 
+                Maar daar stopt het niet bij:</p>
+                <p>Onze hormonen worden via deze voeding en via de huid bewerkt door deze ‘welvaart’ en welvaartproducten, met als gevolg dat onze natuurlijke immuniteit onder druk staat.</p>
+                <p>We spuiten luchtverfrissers in onze huizen, brengen schadelijke deo en synthetisch parfum op onze huid. Dagelijks nog wel.</p>
+                <p>Nog nooit kenden we zoveel allergische reacties en intolerantie verschijnselen.</p>
+                <p>Maar dat hoeft geen feit te blijven.  En af & toe zondigen tegen de regels van natuurlijkheid in, behoeft geen taboe te zijn. Je kan namelijk kiezen waar je je mee verzorgt. En kleine veranderingen maken wel degelijk een groot verschil.</p>
+                <p>Dàt wat je met regelmaat doet of nalaat maakt juist het grootste verschil uit in the long run.</p>
+                    Met regelmaat verschijnen er hier op deze website kleine workshops op de kalender, die geen voorkennis vereisen. 
+                    Op jouw persoonlijke maat leer je zelf voor je huid te zorgen, op de meest integere manier.
+                    Jouw lichaamshuid intiemer en beter leren kennen is een genot, waar je zelf veel plezier aan zal ondervinden.</p>
+                   
+                <p>Voor meer info kan je me ook volgen op Instagram of Facebook.</p>
+                    
+                <p>Wil je je liever làten verzorgen en verwennen?... Dan kan je de prijslijst van mijn intieme schoonheidssalon raadplegen en op die manier genieten!</p>
+                    
+                <p>Trefwoorden: aromatherapie-ongekunsteld-homeopathie-DYI-genieten-fythotherapie-geuren-echte natuurlijkheid-wellness-happy feel-gezonde hormonen-hoe werkt mijn huid- balans- gezonde ingrediënten-ontspanning-ontstressen-creatief zijn</p>
+
+
+
+
+
+',
+    'herbs' => 'Herbes Naturelles',
+    'pedicure' => 'Pédicure Médicale',
+
+    /* Relaxation */
+    'meditation' => 'Méditation',
+    'coaching' => 'Encadrement',
+    'spirituality' => 'Spiritualité',
+
+    /* Divination */
+    'movie' => 'Film',
+    'price' => 'Tarifs',
+
+    /* Contact */
+    'contact' => 'Contact',
+    'parking' => 'Stationnement',
+    'location' => 'Lieu',
+    'subscribe' => 'Formulaire d\'inscription',
+];
