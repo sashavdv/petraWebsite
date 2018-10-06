949f02560e3ca0e7c5997d1597ef52a20464359f
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-10-06 16:31:35 +0200

    ccs cleanup

--- resources/views/layouts/app-new.blade.php
+++ resources/views/layouts/app-new.blade.php
@@ -16,9 +16,7 @@
         {{ config('app.name') }}
     </title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"/>
-    @yield('styles')
 
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="{{ asset('js/bootstrap.js') }}" defer></script>
@@ -79,6 +77,7 @@
             <span class="copy">&copy; pure-petra.com - {{ date('Y') }}</span>
         </div>
     </footer>
+
     <script>
         function toggleSideNav() {
             var sideNav = document.getElementById('side-nav');
