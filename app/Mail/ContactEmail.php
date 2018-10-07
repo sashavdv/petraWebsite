19356f47be7985eaf45c07d580c5a36ba64abed3
yoerioptr <37267734+yoerioptr@users.noreply.github.com>
2018-10-07 11:36:02 +0200

    Created Mailables (Info & Contact)

---
+++ app/Mail/ContactEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ContactEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $aMailData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($aMailData)
+    {
+        $this->aMailData = $aMailData;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(['address' => config('mail.username'), 'name' => config('app.name')])
+                    ->replyTo(['address' => $this->aMailData->from, 'name' => $this->aMailData->name])
+                    ->subject($this->aMailData->subject)
+                    ->view('emails.contact.message')
+                    ->text('emails.contact.message_plain');
+    }
+}
---
+++ app/Mail/InfoEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class InfoEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $aMailData;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($aMailData)
+    {
+        $this->aMailData = $aMailData;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from(['address' => config('mail.username'), 'name' => config('app.name')])
+            ->subject($this->aMailData->subject)
+            ->view('emails.info.message')
+            ->text('emails.info.message_plain');
+    }
+}
