0d7299fde4b1b0f0ea38f3bc1b5c6ae8dd942ae3
sashavdv <sashavandevoorde@gmail.com>
2018-12-13 16:07:11 +0100

    added some text to relaxation

--- resources/lang/nl/content.php
+++ resources/lang/nl/content.php
@@ -8,4 +8,96 @@ return [
     'parking-free' => 'Gratis',
     'parking-paid' => 'Betalend',
     'parking-disclaimer' => 'Tarieven kunnen in de loop van het jaar verandern. Informeer u zeker op voorhand.',
-];
+    'relaxation-paragraph-1' => 'In een wereld waar we overspoeld worden door vervuiling, lichtpollutie, welvaart- toxiciteit, stress en een overvloed aan prikkels, krijgen onze organen en ons zenuwstelsel het zwaar te verduren.',
+    'relaxation-paragraph-2' => 'Het is maar logisch dat het ons soms allemaal te machtig en teveel wordt. Dit kan je enerzijds zien aan het stijgend aantal burn-outs en anderzijds aan het aantal mensen dat ervoor kiest om volledig anders te gaan leven.',
+    'relaxation-paragraph-3' => 'Ontspanning is dus broodnodig en dat kan je op verschillende manieren doen. Er is echter een groot verschil tussen gewoon een wijntje gaan drinken met vrienden, een avondje voor de tv hangen of het verankeren van een werkelijk rustpunt. 
+    Dit laatste zinkt veel dieper in ons lichaamssysteem in en werkt veel langer door.',
+    'relaxation-paragraph-4' => 'Toch is het niet nodig om je leven volledig om te gooien om meer balans te ervaren. Bovendien kan je balancering of herbalancering op elk moment van de dag doorvoeren.',
+    'relaxation-paragraph-5' => '… als het je allemaal teveel wordt.
+… als je je beter in je vel wilt voelen.
+… als je snel boos wordt.
+… als je het gevoel hebt dat je leven te snel gaat.
+… als je je somber voelt.
+… als je op zoek bent naar een meer completer leven.
+… als je voelt dat je rust mist.
+… als je je bewustzijn wilt verruimen.
+… als je merkt dat je sarcastisch en kortaf naar anderen reageert.
+… als je jezelf gelukkig wilt maken zonder dat het al teveel van je vergt.
+
+Het is je innerlijke bron van geluk naar buiten laten komen en niemand kan dat van je afnemen of beïnvloeden als jij dat niet wilt.
+',
+    'relaxation-paragraph-6' => 'Balanceren of relaxatie kan je leren, en meditatie is daarbij een doeltreffende techniek die ik je kan aanleren.
+Evenzo kan je je huidige leven naast dat van de levensgebieden van de Chakra’s leggen. 
+Ook hier bied ik een programma voor aan. Het ene al meer uitgebreid als het andere, steeds op maat van je eigen specifieke behoeftes en wensen.
+',
+    'relaxation-paragraph-7' => 'Meditatie, ontspanningstechnieken, gezonde slaap, een frisse geest in een verfrist lijf, meer balans in AL je levensgebieden doordat jij ze onder de loupe neemt – wil jij dit ook?...
+Dat kan!',
+    'relaxation-paragraph-8' => 'Wil jij ook leren hoe je meer balans in je leven kan aanbrengen?
+Kijk dan met regelmaat naar de workshop/activiteiten agenda op deze website, of volg me via Instagram, YouTube of Facebook.
+',
+    'relaxation-paragraph-9' => 'Laten we samen op pad gaan! <3
+
+
+BIJLAGE bij MEDITATIE:
+
+→ Onze oerinstincten werken door.
+Al 20.000 jaar ontwikkelen wij ons als mens, toch blijven onze zelfbehoud reflexen gelden. Je hoorde waarschijnlijk al van de vecht-vlucht-verstijven (fight-flight-freeze) reactie. 
+
+Die benaming omvat datgene wat er in ons oudste hersendeel gebeurt wanneer we een bedreiging ervaren.
+
+Zeer nuttig om dit zo te hebben werken als je nog als jager in de prehistorie leeft en een gevecht op leven en dood moet aangaan met een groot wild dier of een vijandige stam, maar praktisch onhoudbaar als je je in onze huidige maatschappij begeeft waar diezelfde zelfbehoud reflexen geactiveerd worden omdat er een emotionele behoefte niet vervuld wordt.
+
+Dit lymbisch systeem (oudste hersendeel) dat jouw emoties, reuk, gedrag en zintuigen aanstuurt, triggert telkens weer je autonome zenuwstelsel dat op zijn beurt je hormonenhuishouding en je stofwisseling beheerst.
+Zelfs je sympatische zenuwstelsel ( deel van het autonome zenuwstelsel dat instaat voor je lichaamsprocessen) reageert dan op volle toeren.
+
+→ Lichamelijke gevolgen van onze zelfbehoud reflexen:
+hogere bloeddruk en je hart staat onder druk
+bloedvaten vernauwen zich
+stresshormonen nemen toe, verhoogde staat van lichamelijke alertheid
+torenhoge bloedsuikerspiegel 
+de spijsvertering valt stil doordat je bloedsomloop voornamelijk naar je benen en armen toegaat om te kunnen aanvallen of vluchten
+groei én seksuele hormonen nemen pauze
+immuniteit wordt onderdrukt, ziekteverwekkers bestrijden is overbodig als je leven ‘bedreigd’ wordt
+je bloed wordt kleveriger doordat je lichaam zich op verwondingen voorbereid
+
+AL deze zaken voert je lijf volautomatisch uit, met als gevolg een verhoogd risico op vele stress gerelateerde ziekten. Zelfs ons leervermogen en ons geheugen tast stress aan.
+
+Maar het voornaamste wat te onthouden valt van dit alles, is dat in onze huidige maatschappij stress veroorzaakt wordt door onvervulde behoeften!
+Elke ergernis, elke tegenvaller, elke druk van werk of je omgeving jij ervaart, zet deze oude vecht-vlucht-verstijven modus in actie!
+
+Klinische proeven wijzen uit dat een  gemiddeld persoon die niét overwerkt is, of aan het einde van zijn Latijn zit, toch nog gemiddeld 11 à 12 keer per dag dit proces doorgaat door één of andere kleine frustratie. 
+Dit komt uit op een gemiddelde van meer dan 1 keer per uur!
+
+Reden ten over dus, om een manier te vinden om dit proces trachten om te keren. Liefst dan nog cumulatief werkend elke maal je je gekozen methode beoefent.
+
+Meditatie lijkt de methode bij uitstek om dit aan te pakken.
+“Ga je het internet op vind je al makkelijk over de 7.000.000 zoekresultaten op meer dan 2000 onderzoeksdocumenten die dit staven.” (* de Geheimen van Meditatie – Davidji)
+
+Verlichting van stress, fysiek welbehagen en op een goede manier cognitief functioneren,…allemaal wordt dit via klinische studies rechtstreeks in verband gebracht met mediteren.
+
+Ook is mediteren iets wat iedereen kan. De transformerende resultaten werken al van je eerste sessie. 
+
+Alleen het daadwerkelijk doen is aan jou.
+
+
+
+
+BIJLAGE bij CHAKRA-werk:
+
+De Chakra’s vertegenwoordigen een verzameling van verschillende energie centra in ons lichaam. Eigenlijk kan je ze beschouwen als een soort van brug tussen materie en geest. 
+
+Een chakra behuist een plaats in ons lichaam, met zijn eigen specifieke orgaan dat bepaalde hormonale en zenuw gerelateerde fysieke processen beheerst. Ook linken we een chakra dan telkens aan een levensgebied en specifieke behoeften, emoties en ervaringen.
+
+De meest klassieke methode reikt je 7 levensgebieden aan die het best in balans verkeren, wil jij je voldaan en gelukkig in je vel voelen. Deze levensgebieden kennen we uit de overlevering van de tantrische yoga traditie. Hier en nu in het Westen, brengen we ze nog altijd in verband met subtiele draaikolken van energie, die de zenuwknopen van ons centrale zenuwstelsel aansturen en beïnvloeden.
+
+Ook worden de chakra’s in verband gebracht met een spiritueel ontwaken, al kan zulks natuurlijk ook beschouwd worden als een puur lichamelijke ervaring.
+Mijn inziens geldt het beiden. 
+
+En wat ik meer belangrijk vind:
+ “de Chakra’s kan je gebruiken als een vergrootglas om naar jezelf en de wereld om je heen te kijken.” (* Werkboek Chakra – Anodea Judith)
+
+Wil jij ook meer balans in je leven ondervinden, kijken waar het beter kan of waar het nog aan schort?... 
+Dan is Chakra-werk iets voor jou. Het feit dat je deze tekst leest, is al een 1ste indicatie dat je de universele synchronisiteit ervaart, die je kan toelaten dit pad te nemen.
+
+Ik wens je in alle liefde hier succes toe!',
+    ];
---
+++ resources/lang/nl/subtitles.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sasha
+ * Date: 18/11/2018
+ * Time: 13:01
+ */
+
+return [
+    'relaxation-subtitle-1' => 'Bedreigingen van de innerlijke vrede, belang van relaxatie.',
+    'relaxation-subtitle-2' => 'Wanneer kiezen voor herbalancerende relaxatie?',
+    'relaxation-subtitle-3' => 'Wat kan ik voor je betekenen?',
+];
