4ffd633770bf1ab76c101e8859a501ae6249ee24
yoerioptr <yoeri.optroodt@gmail.com>
2019-01-05 12:04:43 +0100

    Wider images beauty

--- resources/views/user/pages/beauty.blade.php
+++ resources/views/user/pages/beauty.blade.php
@@ -15,8 +15,8 @@
             </div>
             <div class="mt-30">
                 <div class="ui middle aligned stackable grid container">
-                    <div class="fourteen column row">
-                        <div class="six wide column">
+                    <div class="sixteen column row">
+                        <div class="eight wide column">
                             <img class="ui image bordered rounded full-width-image" src="{{ asset('images/beauty/Beauty_Behandeling_penselen.jpg') }}" alt="A picture of a beauty treatment">
                         </div>
                         <div class="eight wide column middle aligned right floated">
@@ -25,39 +25,52 @@
                         </div>
                     </div>
 
-                    <div class="fourteen column row">
+                    <div class="ui divider"></div>
+
+                    <div class="sixteen column row">
                         <div class="eight wide column middle aligned">
                             <p>@lang('content.beauty_paragraph_2')</p>
                         </div>
-                        <div class="six wide column right floated ">
+                        <div class="eight wide column right floated ">
                             <img  class="ui image bordered rounded medium full-width-image"  src="{{ asset('images/beauty/beauty_gelaat.jpg') }}" alt="A picture of petra stienaers">
                         </div>
                     </div>
-                    <div class="fourteen column row">
-                        <div class="six wide column">
+
+                    <div class="ui divider"></div>
+
+                    <div class="sixteen column row">
+                        <div class="eight wide column">
                             <img  class="ui image bordered rounded full-width-image"  src="{{ asset('images/beauty/beauty_3.jpg') }}" alt="A picture of relaxed customer">
                         </div>
                         <div class="eight wide column right floated middle aligned">
                             <p>@lang('content.beauty_paragraph_3')</p>
                         </div>
                     </div>
-                    <div class="fourteen column row">
+
+                    <div class="ui divider"></div>
+
+                    <div class="sixteen column row">
                         <div class="eight wide column middle aligned">
                             <p>@lang('content.beauty_paragraph_4')</p>
                         </div>
-                        <div class="six wide column right floated ">
+                        <div class="eight wide column right floated ">
                             <img  class="ui image bordered rounded full-width-image"  src="{{ asset('images/beauty/beauty_Voetenbad.jpg') }}" alt="A picture of feetbath">
                         </div>
                     </div>
-                    <div class="fourteen column row">
-                        <div class="six wide column">
-                            <img  class="ui image bordered rounded large" src="{{ asset('images/beauty/beauty_Behandeling_man.jpg') }}" alt="A picture of a man getting a massage">
+
+                    <div class="ui divider"></div>
+
+                    <div class="sixteen column row">
+                        <div class="eight wide column">
+                            <img  class="ui image bordered rounded full-width-image" src="{{ asset('images/beauty/beauty_Behandeling_man.jpg') }}" alt="A picture of a man getting a massage">
                         </div>
                         <div class="eight wide column right floated middle aligned">
                             <p>@lang('content.beauty_paragraph_5')</p>
                         </div>
                     </div>
+
                     <div class="ui divider"></div>
+
                     <div class="sixteen wide row ">
                         <div class="image-row">
                             <img src="{{ asset('images/logos/NATRUE-label.jpg') }}">
@@ -65,8 +78,6 @@
                             <img src="{{ asset('images/beauty/Aroma.jpg') }}">
                             <img src="{{ asset('images/beauty/pedicure2.jpg') }}">
                         </div>
-                        <style>
-                        </style>
                     </div>
                 </div>
             </div>
